70735538b2bc59332261bff8e827e065a09d9065
daxas-boop <daxas.swd@hotmail.com>
2020-07-08 23:07:32 -0300

    Agrega pruebas

--- cypress/integration/simon-dice.js
+++ cypress/integration/simon-dice.js
@@ -1,9 +1,15 @@
-const URL = "http://192.168.1.52:8080/";
-it("Visita la pagina", () =>{
-    cy.visit(URL)
-    console.log("help")
-})
+const URL = "http://127.0.0.1:8080/";
 
 it("Prueba el juego", () =>{
+    cy.visit(URL)
+    cy.get(".cuadro").should("have.css" ,"opacity", "0.3")
     cy.get("#Empezar").click();
-})
+
+
+
+
+cy.get('.cuadro')
+  .should(($div) => {
+    expect($div).to.have.css("opacity", "1")
+  })
+});
